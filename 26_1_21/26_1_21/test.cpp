7ed17518e070717c464a5dd149acfb0fa50c16ae
wang-he717 <353380648@qq.com>
2026-01-25 22:40:07 +0800

    更新了有关不可继承类的内容

--- 26_1_21/26_1_21/test.cpp
+++ 26_1_21/26_1_21/test.cpp
@@ -37,85 +37,101 @@ using namespace std;
 //	p.eat();
 //}
 
-class Person
+//class Person
+//{
+//public:
+//	Person(const char* name = "peter")
+//		:_name(name)
+//	{
+//		cout << "Person(const char* name)"<<endl;
+//	}
+//	Person(const Person& p)
+//		:_name(p._name)
+//	{
+//		cout << "Person(const Person& p)"<<endl;
+//	}
+//	Person& operator=(const Person& p)
+//	{
+//		cout << "	Person& operator=(const Person& p)"<<endl;
+//		if (this != &p)
+//		{
+//			_name = p._name;
+//		}
+//		return *this;
+//	}
+//	~Person()
+//	{
+//		cout << "~Person()" << endl;
+//	}
+//	static int _count;
+//	string _name;
+//};
+//
+//int Person::_count = 1;
+//
+//class Student :public Person
+//{
+//public:
+//	Student(const char* name,int num)
+//		:Person(name)
+//		,_num(num)
+//	{
+//		cout << "student(const char* name,const int num)"<<endl;
+//	}
+//	Student(const Student& s)
+//		:Person(s)
+//		,_num(s._num)
+//	{
+//		cout << "Student(const Student& s)"<<endl;
+//	}
+//	Student& operator=(const Student& s) 
+//	{
+//		cout << "Student& operator=(const Student& s) "<<endl;
+//		if (this != &s) {
+//			Person::operator=(s);
+//			_num = s._num;
+//		}
+//		return *this;
+//	}
+//	~Student()//先调用派生类析构函数再自动调用基类析构
+//	{
+//		cout << "~Student()" << endl;
+//	}
+//protected:
+//	int _num=99;
+//};
+//
+//int main()
+//{
+//	Person p;
+//	Student s("ww",22);
+//	cout << &p._name << endl;
+//	cout << &s._name << endl;
+//
+//	cout << &p._count << endl;
+//	cout << &s._count << endl;//派生类可继承基类的静态成员
+//
+//	cout << Person::_count << endl;
+//	cout << Student::_count << endl;
+//
+//	Student s1("www", 44);
+//	s1 = s;
+//	cout << s1._name<<endl;
+//	return 0;
+//}
+
+//实现一个不可继承的类，方法1：
+class Base final
 {
 public:
-	Person(const char* name = "peter")
-		:_name(name)
-	{
-		cout << "Person(const char* name)"<<endl;
-	}
-	Person(const Person& p)
-		:_name(p._name)
-	{
-		cout << "Person(const Person& p)"<<endl;
-	}
-	Person& operator=(const Person& p)
-	{
-		cout << "	Person& operator=(const Person& p)"<<endl;
-		if (this != &p)
-		{
-			_name = p._name;
-		}
-		return *this;
-	}
-	~Person()
-	{
-		cout << "~Person()" << endl;
-	}
-	static int _count;
-	string _name;
-};
-
-int Person::_count = 1;
-
-class Student :public Person
-{
-public:
-	Student(const char* name,int num)
-		:Person(name)
-		,_num(num)
-	{
-		cout << "student(const char* name,const int num)"<<endl;
-	}
-	Student(const Student& s)
-		:Person(s)
-		,_num(s._num)
-	{
-		cout << "Student(const Student& s)"<<endl;
-	}
-	Student& operator=(const Student& s) 
-	{
-		cout << "Student& operator=(const Student& s) "<<endl;
-		if (this != &s) {
-			Person::operator=(s);
-			_num = s._num;
-		}
-		return *this;
-	}
-	~Student()//先调用派生类析构函数再自动调用基类析构
-	{
-		cout << "~Student()" << endl;
-	}
+	void func() { cout << "func()" << endl; }
 protected:
-	int _num=99;
+	int _a = 2;
+//C++98的方法2:
+// 	   这里主要体现了private与protected的区别
+//private:
+//	Base()
+//	{
+//
+//	}
 };
-
-int main()
-{
-	Person p;
-	Student s("ww",22);
-	cout << &p._name << endl;
-	cout << &s._name << endl;
-
-	cout << &p._count << endl;
-	cout << &s._count << endl;//派生类可继承基类的静态成员
-
-	cout << Person::_count << endl;
-	cout << Student::_count << endl;
-
-	Student s1("www", 44);
-	s1 = s;
-	cout << s1._name<<endl;
-	return 0;
-}

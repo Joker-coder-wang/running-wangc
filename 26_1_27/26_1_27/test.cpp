b7ceff00cdecb9f729da65a2e1e216c794e44800
wang-he717 <353380648@qq.com>
2026-01-27 21:12:47 +0800

    关于多态运行原理

--- 26_1_27/26_1_27/test.cpp
+++ 26_1_27/26_1_27/test.cpp
@@ -10,6 +10,7 @@ public:
 	{
 		cout << "全价票" << endl;
 	}
+	void func1(){}
 };
 
 class Student:public Person
@@ -20,22 +21,51 @@ public:
 		cout << "半价票" << endl;
 	}
 };
+//
+//void func(Person& ptr) //实现多态效果第一必须是基类的指针或引用，因为只有基类的指针或引用才能既指向基类对象又指向派生类对象
+//{
+//	ptr.BuyTicket();//由ptr指向的对象来调控谁来使用BuyTicket()函数
+//}
+//
+//int main()
+//{
+//	Person p;
+//	Student s;
+//
+//	//p.BuyTicket();
+//	//s.BuyTicket();
+//
+//	func(p);
+//	func(s);
+//
+//	return 0;
+//}
 
-void func(Person& ptr) //实现多态效果第一必须是基类的指针或引用，因为只有基类的指针或引用才能既指向基类对象又指向派生类对象
+
+int main() 
 {
-	ptr.BuyTicket();//由ptr指向的对象来调控谁来使用BuyTicket()函数
-}
+	int i = 0;
+	static int j = 1;
+	int* p1 = new int;
+	const char* p2 = "xxxxxxxx";
+	printf("栈:%p\n", &i);
+	printf("静态区:%p\n", &j);
+	printf("堆:%p\n", p1);
+	printf("常量区:%p\n", p2);
 
-int main()
-{
-	Person p;
-	Student s;
 
-	//p.BuyTicket();
-	//s.BuyTicket();
+	//检验虚函数所在位置为常量区
+	//虚函数表，简称虚表，本质是一个函数指针数组
+	Person ps;
+	Person* ptr = &ps;//Person对象的前4字节为虚表地址，那么我们可以取Person的地址，并强转为int*类型
+	printf("虚表：%p\n", *((int*)ptr));
+	Student st;
+	printf("虚表：%p\n", *((int*)&st));
 
-	func(p);
-	func(s);
+
+	//此处体现虚函数地址与普通函数离得不远
+	printf("虚函数地址:%p\n", &Person::BuyTicket);
+	printf("普通函数地址:%p\n", &Student::func1);
 
 	return 0;
 }

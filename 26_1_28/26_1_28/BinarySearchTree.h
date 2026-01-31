3d7555dde36e3ef2e24da6c93b1d43806c2370d5
wang-he717 <353380648@qq.com>
2026-01-31 21:44:19 +0800

    修改

--- 26_1_28/26_1_28/BinarySearchTree.h
+++ 26_1_28/26_1_28/BinarySearchTree.h
@@ -1,8 +1,9 @@
 #pragma once
 #include<iostream>
+#include<string>
 using namespace std;
 
-namespace wh {
+namespace key {
 	template<class K>
 	struct BSTNode
 	{
@@ -198,4 +199,189 @@ namespace wh {
 			_InOrder(root->_right);
 		}
 	};
+}
+
+namespace key_value
+{
+	template<class K,class V>
+	struct BSTNode
+	{
+		K _key;
+		V _value;
+		BSTNode<K,V>* _left;
+		BSTNode<K,V>* _right;
+
+		BSTNode(const K& key,const V& value)
+			:_key(key)
+			,_value(value)
+			, _left(nullptr)
+			, _right(nullptr)
+		{
+		}
+	};
+
+
+	template<class K,class V>
+	struct BSTree
+	{
+		typedef BSTNode<K,V> Node;
+
+		BSTree() = default;
+		BSTree<K, V>& operator=(BSTree<K, V> t)
+		{
+		swap(_root, t._root);
+		return *this;
+		}
+
+
+		bool Insert(const K& key,const V& value)
+		{
+			if (_root == nullptr)
+			{
+				_root = new Node(key,value);
+			}
+			Node* parent = nullptr;
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;//防止函数栈帧销毁之后走丢
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else {//此时为key == cur->_key
+					return false;
+				}
+			}
+			cur = new Node(key,value);//此时cur为空
+			if (parent->_key < key)
+			{
+				parent->_right = cur;
+			}
+			else {
+				parent->_left = cur;
+			}
+
+			return true;
+		}
+
+		Node* Find(const K& key)
+		{
+			Node* cur = _root;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					cur = cur->_left;
+				}
+				else {
+					return cur;
+				}//原来错在这里！
+			}
+			return nullptr;
+		}
+
+		bool Erase(const K& key)
+		{
+			if (_root == nullptr)
+				return false;
+			Node* cur = _root;
+			Node* parent = nullptr;
+			while (cur)
+			{
+				if (cur->_key < key)
+				{
+					parent = cur;
+					cur = cur->_right;
+				}
+				else if (cur->_key > key)
+				{
+					parent = cur;
+					cur = cur->_left;
+				}
+				else {
+					//找到了key，现在进行删除操作
+					if (cur->_left == nullptr)
+					{
+						//左为空，让父亲指向我的右
+						if (cur == parent->_left)
+						{
+							parent->_left = cur->_right;
+						}
+						else
+						{
+							parent->_right = cur->_right;
+						}
+						delete cur;
+					}
+					else if (cur->_right == nullptr) {
+						//右为空，让父亲指向我的左
+						if (cur == parent->_left)
+						{
+							parent->_left = cur->_left;
+						}
+						else
+						{
+							parent->_right = cur->_left;
+						}
+						delete cur;
+					}
+					else {
+						//左右都不为空，找到合适子树替代我
+						//找一个节点，比左大，比右小来替代cur
+						//由于二叉搜索树的性质，插入值比当前节点大的往右走，插入值比当前节点小的往左走
+						Node* minRightParent = cur;
+						Node* minRight = cur->_right;
+						//Node* minRightParent = nullptr;//假设minRight没有左节点，那么minRight就不会更新
+						//所以此处要设置minRightParent = cur；
+						while (minRight->_left)
+						{
+							minRightParent = minRight;
+							minRight = minRight->_left;
+						}
+						cur->_key = minRight->_key;//swap(minRight->_key,cur->_key);
+						if (minRight == minRightParent->_left)
+							minRightParent->_left = minRight->_right;
+						else
+							minRightParent->_right = minRight->_right;
+						//因为此处minRightParent->_left为空,
+						//且此时minRight->_left一定为空,但right不一定为空，一定要进行处理！！
+						//处理方式就是把他给minRight的左子树
+
+						delete minRight;
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		void InOrder()
+		{
+			_InOrder(_root);
+			cout << endl;
+		}
+
+	private:
+		Node* _root = nullptr;
+
+		void _InOrder(Node* root)
+		{
+			if (root == nullptr)
+				return;
+			_InOrder(root->_left);
+			cout << root->_key << " ";
+			_InOrder(root->_right);
+		}
+	};
+
 }
--- 26_1_28/26_1_28/test.cpp
+++ 26_1_28/26_1_28/test.cpp
@@ -1,20 +1,42 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include"BinarySearchTree.h"
 
+//int main()
+//{
+//	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+//	key::BSTree<int> t1;
+//
+//	for (auto e : a) {
+//		t1.Insert(e);
+//	}
+//	t1.InOrder();
+//
+//	t1.Erase(8);
+//	t1.InOrder();
+//
+//
+//	return 0;
+//}
 int main()
 {
-	int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
-	wh::BSTree<int> t1;
+	key_value::BSTree<string, string> dict;
+	dict.Insert("left", "×ó±ß");
+	dict.Insert("right", "ÓÒ±ß");
+	dict.Insert("insert", "²åÈë");
+	dict.Insert("string", "×Ö·û´®");
 
-	for (auto e : a) {
-		t1.Insert(e);
+	string str;
+	while (cin >> str)
+	{
+		auto ret = dict.Find(str);
+		if (ret)
+		{
+			cout << "->" << ret->_value << endl;
+		}
+		else {
+			cout << "ÎÞ´Ëµ¥´Ê£¬ÇëÖØÐÂÊäÈë£¡" << endl;
+		}
 	}
-	t1.InOrder();
-
-	t1.Erase(8);
-	t1.InOrder();
-
 
 	return 0;
 }
-
